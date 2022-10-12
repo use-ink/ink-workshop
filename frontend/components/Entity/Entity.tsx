0afed75e111945b4437f9a758431112c59d42040
Sam Ruberti <samruberti@gmail.com>
2022-10-12 11:10:47 -0400

    Add animations to sea creatures (#11)

--- frontend/components/Entity/Entity.tsx
+++ frontend/components/Entity/Entity.tsx
@@ -1,12 +1,32 @@
 /* eslint-disable @next/next/no-img-element */
 import classNames from 'classnames';
+import { animated, useSpring, config, easings } from 'react-spring';
 
 type Props = {
   src: string;
   alt: string;
   className?: string;
+  bob?: number;
+  sway?: number;
+  duration?: number;
+  startLeft?: boolean;
+  startDown?: boolean;
 };
 
-export const Entity: React.FC<Props> = ({ src, className, alt }) => {
-  return <img src={src} className={classNames('w-12', className)} alt={alt} />;
+export const Entity: React.FC<Props> = ({ src, className, alt, bob, sway, startLeft, duration, startDown }) => {
+  const halfSway = (sway || 0) / 2;
+  const startSway = startLeft ? -halfSway : halfSway;
+  const endSway = startLeft ? halfSway : -halfSway;
+
+  const halfBob = (bob || 0) / 2;
+  const startBob = startDown ? -halfBob : halfBob;
+  const endBob = startDown ? halfBob : -halfBob;
+
+  const springProps = useSpring({
+    loop: { reverse: true },
+    from: { y: startBob, x: startSway },
+    to: { y: endBob || 0, x: endSway },
+    config: { duration: duration || 2500, ...config.molasses, easing: easings.easeInOutSine },
+  });
+  return <animated.img src={src} className={classNames('w-12', className)} alt={alt} style={springProps} />;
 };
