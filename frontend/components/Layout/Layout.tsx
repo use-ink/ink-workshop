1363d4e3dfc3391c859033d02865d15bb4d83934
Sam Ruberti <samruberti@gmail.com>
2022-10-12 10:08:21 -0400

    Add animations to sea creatures

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -13,18 +13,36 @@ export const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="bg-sea bg-cover bg-no-repeat bg-bottom fixed top-0 left-0 right-0 bottom-0 w-full h-screen">
       <Nav />
-      <Entity
-        alt="plant"
-        src="/plant-2.svg"
-        className="absolute left-[5%] bottom-[-1%] w-[15%] min-w-[150px] hidden md:block"
-      />
-      <Entity
-        alt="plant"
-        src="/plant-1.svg"
-        className="absolute right-[5%] bottom-0 md:w-[35%] w-[80%] max-w-[500px] min-w-[350px]"
-      />
-      <Entity alt="fish" src="/fish-1.svg" className="absolute left-[2%] xl:bottom-[35%] bottom-[15%] w-20" />
-      <Entity alt="Jelly" src="/jelly-fish.svg" className="absolute right-[3%] xl:top-[34%] top-[15%] md:w-32 w-20" />
+      <div>
+        <Entity
+          sway={15}
+          alt="plant"
+          duration={2000}
+          src="/plant-2.svg"
+          className="absolute left-[5%] bottom-[-1%] w-[15%] min-w-[150px] hidden md:block"
+        />
+        <Entity
+          sway={10}
+          startLeft
+          duration={1500}
+          alt="plant"
+          src="/plant-1.svg"
+          className="absolute right-[5%] bottom-0 md:w-[35%] w-[80%] max-w-[500px] min-w-[350px]"
+        />
+        <Entity
+          bob={20}
+          alt="fish"
+          src="/fish-1.svg"
+          className="absolute left-[2%] xl:bottom-[35%] bottom-[15%] w-20"
+        />
+        <Entity
+          bob={20}
+          startDown
+          alt="Jelly"
+          src="/jelly-fish.svg"
+          className="absolute right-[3%] xl:top-[34%] top-[15%] md:w-32 w-20"
+        />
+      </div>
       <div className="">{children}</div>
     </div>
   );
