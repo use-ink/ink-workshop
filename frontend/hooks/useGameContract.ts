657bffddf29b8993819fb469df5b72071214d86c
Sam Ruberti <samruberti@gmail.com>
2022-10-17 16:16:18 -0400

    Change player colors list

--- frontend/hooks/useGameContract.ts
+++ frontend/hooks/useGameContract.ts
@@ -120,12 +120,12 @@ export const useGameState = (): GameState | null => {
 const PLAYER_COLORS = [
   '#6effdb',
   '#ff705e',
-  '#50d985',
   '#5ea4ff',
   '#f3ff73',
   '#f9e3ff',
   '#f694ff',
   '#ffc32b',
+  '#50d985',
   '#3853ff',
   '#bba4c2',
 ];
