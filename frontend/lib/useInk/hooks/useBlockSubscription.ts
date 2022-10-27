517f5b765333b0c529f8ebe279d0650247b30782
Sam Ruberti <samruberti@gmail.com>
2022-10-27 05:18:51 +0200

    Fix useSubscription trigger for useEffect

--- frontend/lib/useInk/hooks/useBlockSubscription.ts
+++ frontend/lib/useInk/hooks/useBlockSubscription.ts
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
+import { DependencyList, useEffect } from 'react';
 import { useInk } from '../InkProvider';
 
-export const useBlockSubscription = (fn: VoidFunction) => {
+export const useBlockSubscription = (fn: VoidFunction, deps?: DependencyList) => {
   const { header } = useInk();
 
   useEffect(() => {
     header && fn();
-  }, [header?.number?.toHuman(), fn.toString]);
+  }, [header?.number?.toHuman(), fn, deps]);
 };
