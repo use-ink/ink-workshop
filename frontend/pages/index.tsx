0a869bf5f04ab7bc360ce706af33e7bff4444964
lean-apple <lea@parity.io>
2023-01-17 12:24:39 +0000

    add language selector on welcome page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { Layout } from '../components/Layout';
 import { useUI } from '../contexts/UIContext';
+import { LanguageSelect } from './../components/pg-game/GameBoard/Settings/LanguageSelect';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
@@ -32,24 +33,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="h-screen w-full flex items-center justify-center p-6">
-        <div className="bg-players-4 rounded-2xl px-8 pt-12 pb-10 text-center max-w-2xl w-full drop-shadow-xl">
+        <div className="bg-players-4 rounded-2xl px-8 py-10 text-center max-w-2xl w-full drop-shadow-xl">
           <h6 className="text-xl font-semibold">{t('title')}</h6>
           <p className="text-sm mt-3">{t('description')}</p>
           <input
             onChange={(e) => setAddress(e.target.value)}
-            className="rounded-2xl bg-players-6 mt-12 flex items-center justify-center text-center min-h-12 w-full py-4 px-6 focus:outline-none focus:ring-4 focus:ring-players-9"
+            className="rounded-2xl bg-players-6 mt-6 mb-4 flex items-center justify-center text-center min-h-12 w-full py-4 px-6 focus:outline-none focus:ring-4 focus:ring-players-9"
             placeholder={t('gameAddressInput') || ''}
             value={address}
           />
+          <h6 className="text-md font-semibold mt-6">{t('selectLanguage')}</h6>
+          <LanguageSelect className='my-4'/>
           <button
-            className="font-fred w-full mt-6 rounded-2xl py-4 px-6 bg-players-8 hover:bg-players-8/80 drop-shadow-md transition duration-200 focus:outline-none focus:ring-4 focus:ring-players-9"
+            className="font-fred w-full mt-4 rounded-2xl py-4 px-6 bg-players-8 hover:bg-players-8/80 drop-shadow-md transition duration-200 focus:outline-none focus:ring-4 focus:ring-players-9 text-lg"
             onClick={() => address && router.push(`/game/${address}`)}
           >
             {t('gameDescButton')}
           </button>
-
           <button
-            className="text-white font-fred w-full mt-3 rounded-2xl py-4 px-6 bg-brand-500 hover:bg-brand-500/80 drop-shadow-md transition duration-200 focus:outline-none focus:ring-4 focus:ring-players-9"
+            className="text-white font-fred w-full mt-3 rounded-2xl py-4 px-6 bg-brand-500 hover:bg-brand-500/80 drop-shadow-md transition duration-200 focus:outline-none focus:ring-4 focus:ring-players-9 text-lg"
             onClick={() => setShowRules(true)}
           >
             {t('learnDescButton')}
