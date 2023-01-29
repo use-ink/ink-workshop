15a7d47cb5c76026a1ffc9b099b30924b613fe72
Alexander Theißen <alex.theissen@me.com>
2023-01-29 19:08:12 -0300

    Add script to drive the game

---
+++ game/drive-game.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -euxo pipefail
+for (( ; ; ))
+do
+	cargo contract call\
+		--url wss://rococo-contracts-rpc.polkadot.io\
+		--contract "$1"\
+		--suri "$2"\
+		--message submit_turn\
+		--skip-dry-run\
+		--gas 300000000000\
+		--proof-size 512000\
+		--skip-confirm
+done
