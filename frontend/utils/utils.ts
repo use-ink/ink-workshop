09ee056f2b503963c40e16c4d729b8ba051db11f
Sam Ruberti <samruberti@gmail.com>
2023-02-06 16:13:33 -0500

    Set player colors based on name (#62)
    Convert player name to Hex number, then calculate color index from that.

--- frontend/utils/utils.ts
+++ frontend/utils/utils.ts
@@ -4,3 +4,12 @@ export const truncateHash = (hash?: string, length = 38): string =>
 export function pickOne<T>(messages: T[]): T {
   return messages[new Date().getTime() % messages.length];
 }
+
+export const stringToHex = (w: string): string => {
+  const hex = w
+    .split('')
+    .map((c) => c.charCodeAt(0).toString(16).padStart(2, '0'))
+    .join('');
+
+  return `0x${hex}`;
+};
