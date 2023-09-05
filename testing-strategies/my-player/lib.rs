53e9e9892583be8345fd6b3128a951587d861789
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-09-05 10:06:53 +0200

    Add my player contract

---
+++ testing-strategies/my-player/lib.rs
@@ -0,0 +1,27 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod my_player {
+    #[ink(storage)]
+    pub struct MyPlayer {
+        dimensions: (u32, u32),
+        next_turn: u32,
+    }
+
+    impl MyPlayer {
+        #[ink(constructor)]
+        pub fn new(dimensions: (u32, u32), start: u32) -> Self {
+            Self {
+                dimensions,
+                next_turn: start,
+            }
+        }
+
+        #[ink(message, selector = 0)]
+        pub fn my_turn(&mut self) -> Option<(u32, u32)> {
+            let turn = self.next_turn;
+            self.next_turn += 1;
+            Some((turn % self.dimensions.0, turn / self.dimensions.0))
+        }
+    }
+}
