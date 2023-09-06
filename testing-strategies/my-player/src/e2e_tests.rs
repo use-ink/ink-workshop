bbf0ac990d167bf7f263dd6763aef49b4afb6f23
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-09-06 11:33:12 +0200

    One player simulation

--- testing-strategies/my-player/src/e2e_tests.rs
+++ testing-strategies/my-player/src/e2e_tests.rs
@@ -1,12 +1,16 @@
 use ink_e2e::build_message;
+use squink_splash::{Field, Game as GameRef};
 
 use crate::my_player::MyPlayerRef;
 
 type E2EResult<T> = Result<T, Box<dyn std::error::Error>>;
 
+const DIMENSION: u32 = 4;
+const START: u32 = 1;
+
 #[ink_e2e::test]
 fn instantiation_works(mut client: Client) -> E2EResult<()> {
-    let _contract = client
+    let _ = client
         .instantiate(
             "my-player",
             &ink_e2e::alice(),
@@ -22,9 +26,6 @@ fn instantiation_works(mut client: Client) -> E2EResult<()> {
 
 #[ink_e2e::test]
 fn uses_dummy_strategy_correctly(mut client: Client) -> E2EResult<()> {
-    const DIMENSION: u32 = 4;
-    const START: u32 = 1;
-
     let player_address = client
         .instantiate(
             "my-player",
@@ -52,6 +53,109 @@ fn uses_dummy_strategy_correctly(mut client: Client) -> E2EResult<()> {
             panic!("Should have returned coordinates")
         }
     }
+
+    Ok(())
+}
+
+#[ink_e2e::test(additional_contracts = "../../game/Cargo.toml")]
+fn we_can_simulate_one_player_game(mut client: Client) -> E2EResult<()> {
+    let dimensions = Field {
+        x: DIMENSION,
+        y: DIMENSION,
+    };
+    let forming_rounds = 0;
+    let rounds = 10;
+    let buy_in = 0;
+    let caller = ink_e2e::alice();
+
+    let player_address = client
+        .instantiate(
+            "my-player",
+            &caller,
+            MyPlayerRef::new((DIMENSION, DIMENSION), START),
+            0,
+            None,
+        )
+        .await
+        .expect("Failed to instantiate contract")
+        .account_id;
+
+    let game_address = client
+        .instantiate(
+            "squink_splash",
+            &caller,
+            GameRef::new(dimensions, buy_in, forming_rounds, rounds),
+            0,
+            None,
+        )
+        .await
+        .expect("Failed to instantiate contract")
+        .account_id;
+
+    client
+        .call(
+            &caller,
+            build_message::<GameRef>(game_address)
+                .call(|c| c.register_player(player_address, "Player 1".into())),
+            0,
+            None,
+        )
+        .await
+        .unwrap();
+
+    client
+        .call(
+            &caller,
+            build_message::<GameRef>(game_address).call(|c| c.start_game()),
+            0,
+            None,
+        )
+        .await
+        .unwrap();
+
+    // XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+    client
+        .call(
+            &caller,
+            build_message::<GameRef>(game_address).call(|c| c.state()),
+            0,
+            None,
+        )
+        .await
+        .unwrap();
+
+    for _ in 0..rounds {
+        client
+            .call(
+                &caller,
+                build_message::<GameRef>(game_address).call(|c| c.submit_turn()),
+                0,
+                None,
+            )
+            .await
+            .map_err(|e| println!("{:?}", e))
+            .unwrap();
+
+        client
+            .call(
+                &caller,
+                build_message::<GameRef>(game_address).call(|c| c.state()),
+                0,
+                None,
+            )
+            .await
+            .unwrap();
+    }
+
+    client
+        .call(
+            &caller,
+            build_message::<GameRef>(game_address).call(|c| c.end_game()),
+            0,
+            None,
+        )
+        .await
+        .unwrap();
 
     Ok(())
 }
