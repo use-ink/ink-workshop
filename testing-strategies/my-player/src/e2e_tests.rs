bfd7b9f82a539d216eeb624af04df5dcf6c153d0
Piotr Mikołajczyk <piomiko41@gmail.com>
2023-09-06 10:45:08 +0200

    Recreate strategy test as E2E

--- testing-strategies/my-player/src/e2e_tests.rs
+++ testing-strategies/my-player/src/e2e_tests.rs
@@ -1,3 +1,5 @@
+use ink_e2e::build_message;
+
 use crate::my_player::MyPlayerRef;
 
 type E2EResult<T> = Result<T, Box<dyn std::error::Error>>;
@@ -14,6 +16,42 @@ fn instantiation_works(mut client: Client) -> E2EResult<()> {
         )
         .await
         .expect("Failed to instantiate contract");
+
+    Ok(())
+}
+
+#[ink_e2e::test]
+fn uses_dummy_strategy_correctly(mut client: Client) -> E2EResult<()> {
+    const DIMENSION: u32 = 4;
+    const START: u32 = 1;
+
+    let player_address = client
+        .instantiate(
+            "my-player",
+            &ink_e2e::alice(),
+            MyPlayerRef::new((DIMENSION, DIMENSION), START),
+            0,
+            None,
+        )
+        .await
+        .expect("Failed to instantiate contract")
+        .account_id;
+
+    let call = build_message::<MyPlayerRef>(player_address).call(|player| player.my_turn());
+
+    for turn in START..(DIMENSION * DIMENSION) + START {
+        let result = client
+            .call(&ink_e2e::alice(), call.clone(), 0, None)
+            .await
+            .expect("Failed to get coordinates");
+
+        if let Some((x, y)) = result.return_value() {
+            assert_eq!(x, turn % DIMENSION);
+            assert_eq!(y, turn / DIMENSION % DIMENSION);
+        } else {
+            panic!("Should have returned coordinates")
+        }
+    }
 
     Ok(())
 }
